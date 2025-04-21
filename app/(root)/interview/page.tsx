5427624ee3b50deffeac69a60a87fbb106891b22
Hariom1711 <2003om1711@gmail.com>
2025-04-21 14:56:49 +0530

    added taking interview and genrating Feedback

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,13 +1,14 @@
+import Agent from "@/components/Agent";
+import {getCurrentUser} from "@/lib/actions/auth.action";
 
-import Agent from '@/components/Agent'
-import React from 'react'
+const Page = async () => {
+    const user = await getCurrentUser();
 
-const Page = () => {
     return (
         <>
             <h3>Interview Generation</h3>
 
-            <Agent userName="You" userId="user1" type="generate" />
+            <Agent  userName={user?.name || ''}userId={user?.id} type="generate" />
         </>
     )
 }
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,13 +1,15 @@
-
 import dayjs from 'dayjs';
 import Image from "next/image";
 import {getRandomInterviewCover} from "@/lib/utils";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "@/components/DisplayTechIcons";
+import {getFeedbackByInterviewId} from "@/lib/actions/general.action";
 
-const InterviewCard = ({ interviewId, role, type, techstack, createdAt }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+    const feedback = userId && id
+    ? await getFeedbackByInterviewId({ interviewId: id, userId})
+        : null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
 
@@ -47,8 +49,8 @@ const InterviewCard = ({ interviewId, role, type, techstack, createdAt }: Interv
 
                     <Button className="btn-primary">
                         <Link href={feedback
-                            ? `/interview/${interviewId}/feedback`
-                            : `/interview/${interviewId}`
+                            ? `/interview/${id}/feedback`
+                            : `/interview/${id}`
                         }>
                             {feedback ? 'Check Feedback' : 'View Interview'}
                         </Link>
